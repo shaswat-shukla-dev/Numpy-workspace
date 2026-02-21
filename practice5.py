d9d3a0c00f4c4b8b3569d1b4822e5b74866ec020
shaswat-shukla-dev <shaswat.shukla.dev@gmail.com>
2026-02-21 10:37:58 +0530

    slicing in 2-D array

---
+++ practice5.py
@@ -0,0 +1,9 @@
+# slicing in 2-D Arrays
+
+import numpy as np
+arr=np.array([[1,2,3],[4,5,6]])
+print(arr[1,1:4])
+
+arr2 = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
+print(arr2[0:2, 2])
+print(arr2[0:2,1:4])
