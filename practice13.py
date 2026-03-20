ddac295ac2d88d714154812a4b87077d9c681eea
shaswat-shukla-dev <shaswat.shukla.dev@gmail.com>
2026-03-20 11:45:53 +0530

    practice13

---
+++ practice13.py
@@ -0,0 +1,8 @@
+import numpy as np
+array1=np.array([10,20,30,40,50,4,34,56])
+print(np.mean(array1))
+print(np.median(array1))
+print(np.std(array1))
+print(np.var(array1))
+print(np.min(array1))
+print(np.max(array1))
