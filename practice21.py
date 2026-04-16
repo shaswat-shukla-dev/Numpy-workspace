177e7fe462a34c6a535b455d51a8ade1b0fa0cb9
shaswat-shukla-dev <shaswat.shukla.dev@gmail.com>
2026-04-16 12:21:48 +0530

    find chepest car

--- practice21.py
+++ practice21.py
@@ -13,4 +13,11 @@ name_toyota = df.loc[df['company'] == 'toyota']
 
 print("\nAll cars with Toyota:")
 print(name_toyota)
-print(df.count)
+print(df.count())
+df['price'] = pd.to_numeric(df['price'], errors='coerce')
+df = df.dropna(subset=['price'])
+
+cheapest_cars = df.loc[df.groupby('company')['price'].idxmin()]
+
+print("Cheapest car of each company:\n")
+print(cheapest_cars[['company', 'price']])
